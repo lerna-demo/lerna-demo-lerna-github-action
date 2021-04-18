20d7e217f8eb2ceb48cccd14a99c5eb4a3e729f9
TechG <TechG>
2021-04-19 01:23:26 +0530

    script refactored

--- trigger-github-workflow/entrypoint.sh
+++ trigger-github-workflow/entrypoint.sh
@@ -7,34 +7,32 @@ WORKFLOW_FILENAME=$5
 TEMP_FILE="temp.json"
 SLEEP_INTERVAL=30
 
-echo "Invoking workflow of \"$REPO\" repository with workflow file name \"$WORKFLOW_FILENAME\""
-data="{\"ref\":\"$REF\"}"
-curl \
+trigger_workflow () {
+  echo "Invoking workflow of \"$REPO\" repository with workflow file name \"$WORKFLOW_FILENAME\""
+  data="{\"ref\":\"$REF\"}"
+  curl \
     --silent \
     --request POST \
     --header "Accept: application/vnd.github.v3+json" \
     --header "Authorization: Bearer $GITHUB_TOKEN" \
     "https://api.github.com/repos/${REPO}/actions/workflows/${WORKFLOW_FILENAME}/dispatches" \
     --data ${data}
-echo "Successfully workflow triggered"
+  echo "Successfully workflow triggered"
+}
 
-set -e
+list_workflow_runs () {
+    curl \
+        --silent \
+        --location \
+        --request GET \
+        --header 'Accept: application/vnd.github.v3+json' \
+        --header 'Content-Type: application/json' \
+        --header "Authorization: Bearer $GITHUB_TOKEN" \
+        --header 'cache-control: no-cache' \
+        "https://api.github.com/repos/${REPO}/actions/runs"  > $TEMP_FILE
+}
 
-curl \
-    --silent \
-    --location \
-    --request GET \
-    --header 'Accept: application/vnd.github.v3+json' \
-    --header 'Content-Type: application/json' \
-    --header "Authorization: Bearer $GITHUB_TOKEN" \
-    --header 'cache-control: no-cache' \
-    "https://api.github.com/repos/${REPO}/actions/runs"  > $TEMP_FILE
-        
-RUN_ID=$(jq -r ".workflow_runs | sort_by( .created_at ) | .[-1] | .id" $TEMP_FILE)
-
-echo "Checking conclusion of the last executed run in \"$REPO\" repository:"
-while true; do
- 
+workflow_run_status () {
     curl \
         --silent \
         --location \
@@ -44,6 +42,34 @@ while true; do
         --header "Authorization: Bearer $GITHUB_TOKEN" \
         --header 'cache-control: no-cache' \
         "https://api.github.com/repos/${REPO}/actions/runs/$RUN_ID"  > $TEMP_FILE
+}
+
+list_workflow_runs
+BEFORE_RUN_NUMBER=$(jq -r ".workflow_runs | sort_by( .created_at ) | .[-1] | .run_number" $TEMP_FILE)
+
+trigger_workflow
+
+while true; do
+    list_workflow_runs
+
+    count=$(jq -r ".total_count " $TEMP_FILE)
+ 
+    if [ $count -ne 0 ]; then
+        RUN_NUMBER=$(jq -r ".workflow_runs | sort_by( .created_at ) | .[-1] | .run_number" $TEMP_FILE)
+        if [ $RUN_NUMBER -ne $BEFORE_RUN_NUMBER ]; then
+            break;
+        fi
+    fi
+
+    sleep $SLEEP_INTERVAL
+done
+
+echo "Run Number: $RUN_NUMBER"
+RUN_ID=$(jq -r ".workflow_runs | sort_by( .created_at ) | .[-1] | .id" $TEMP_FILE)
+echo "Run ID: $RUN_ID"
+echo "Checking conclusion of the last executed run in \"$REPO\" repository:"
+while true; do
+    workflow_run_status
     
     JOB_URL=$(jq -r ". | .html_url" $TEMP_FILE)
     STATUS=$(jq -r ". | .status" $TEMP_FILE)
@@ -58,7 +84,7 @@ while true; do
  
     sleep $SLEEP_INTERVAL
 done
-rm $TEMP_FILE || true
+rm $TEMP_FILE || true 
 
 if [ "$CONCLUSION" != "success" ]; then
     exit 1
