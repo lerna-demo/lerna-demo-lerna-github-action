42b37f22a8e237034b7571020cec9970f0352d3a
TechG <TechG>
2021-04-18 16:03:56 +0530

    change in terraform action

--- terraform/entrypoint.sh
+++ terraform/entrypoint.sh
@@ -3,9 +3,10 @@
 set -e
 WORKDIR=$1
 TF_WORKSPACE=$2
+TF_ARGS=$3
 
 cd $WORKDIR
 terraform init
 terraform validate
 terraform workspace select $TF_WORKSPACE || terraform workspace new $TF_WORKSPACE
-terraform apply -auto-approve
+terraform $TF_ARGS
