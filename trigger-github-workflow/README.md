037445f6ec3c1b06c12d2c1a526da65aeb7e3c7c
TechG <TechG>
2021-04-18 23:12:01 +0530

    trigger-github-workflow action added

---
+++ trigger-github-workflow/README.md
@@ -0,0 +1,3 @@
+# lerna-github-action > execute-github-workflow
+
+
---
+++ trigger-github-workflow/entrypoint.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+set -e
+
+GITHUB_TOKEN=$1
+REPO="$2/$3"
+REF=$4
+WORKFLOW_FILENAME=$5
+TEMP_FILE="temp.json"
+SLEEP_INTERVAL=30
+
+echo "Invoking workflow of \"$REPO\" repository with workflow file name \"$WORKFLOW_FILENAME\""
+data="{\"ref\":\"$REF\"}"
+curl \
+    --silent \
+    --request POST \
+    --header "Accept: application/vnd.github.v3+json" \
+    --header "Authorization: Bearer $GITHUB_TOKEN" \
+    "https://api.github.com/repos/${REPO}/actions/workflows/${WORKFLOW_FILENAME}/dispatches" \
+    --data ${data}
+
+echo "Successfully workflow triggered"
+
+curl \
+    --silent \
+    --location \
+    --request GET \
+    --header 'Accept: application/vnd.github.v3+json' \
+    --header 'Content-Type: application/json' \
+    --header "Authorization: Bearer $GITHUB_TOKEN" \
+    --header 'cache-control: no-cache' \
+    "https://api.github.com/repos/${REPO}/actions/runs"  > $TEMP_FILE
+        
+RUN_ID=$(jq -r ".workflow_runs | sort_by( .created_at ) | .[-1] | .id" $TEMP_FILE)
+
+echo "Checking conclusion of the last executed run in \"$REPO\" repository:"
+while true; do
+ 
+    curl \
+        --silent \
+        --location \
+        --request GET \
+        --header 'Accept: application/vnd.github.v3+json' \
+        --header 'Content-Type: application/json' \
+        --header "Authorization: Bearer $GITHUB_TOKEN" \
+        --header 'cache-control: no-cache' \
+        "https://api.github.com/repos/${REPO}/actions/runs/$RUN_ID"  > $TEMP_FILE
+    
+    JOB_URL=$(jq -r ". | .html_url" $TEMP_FILE)
+    STATUS=$(jq -r ". | .status" $TEMP_FILE)
+    echo "Checking run state: ${STATUS}"
+ 
+    if [ "$STATUS" = "completed" ]; then
+        CONCLUSION=$(jq -r ". | .conclusion" $TEMP_FILE)
+        echo "Checking run conclusion: ${CONCLUSION}"
+        break;
+    fi
+ 
+    sleep $SLEEP_INTERVAL
+done
+
+rm $TEMP_FILE || true
+
+if [ "$CONCLUSION" != "success" ]; then
+    exit 1
+fi
