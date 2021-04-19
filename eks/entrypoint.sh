ec945adae3d2c3a0af83584199fcaa49e5d214c8
TechG <TechG>
2021-04-19 14:12:03 +0530

    eks github action updated

--- eks/entrypoint.sh
+++ eks/entrypoint.sh
@@ -13,7 +13,7 @@ if [ "$KUBECTL_ARG" != "" ]; then
     sh -c "kubectl --kubeconfig /tmp/config $KUBECTL_ARG"
 elif [ "$APP_HOST" != "" ]; then
     POD_ID=$(kubectl --kubeconfig /tmp/config get pods -l name=$APP_NAME -n $APP_ENVIRONMENT | tail -1 | awk '{print $1}')
-    HTTP_STATUS=kubectl --kubeconfig /tmp/config exec $POD_ID -n $APP_ENVIRONMENT -- curl -o /dev/null -s -w "%{http_code}\n" $APP_HOST
+    HTTP_STATUS=$(kubectl --kubeconfig /tmp/config exec $POD_ID -n $APP_ENVIRONMENT -- curl -o /dev/null -s -w "%{http_code}\n" $APP_HOST)
     if [ "$HTTP_STATUS" != "200" ]; then
         echo "Application is not healthy, HTTP STATUS CODE = \"$HTTP_STATUS\""
         exit 1
