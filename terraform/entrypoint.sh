687057af3cf1af3c7f57ef6e715832c0a17aec14
TechG <TechG>
2021-04-21 17:05:22 +0530

    retry added in terraform action

--- terraform/entrypoint.sh
+++ terraform/entrypoint.sh
@@ -4,9 +4,26 @@ set -e
 WORKDIR=$1
 TF_WORKSPACE=$2
 TF_ARGS=$3
+RETRY=$4
+SLEEP_INTERVAL=30
 
 cd $WORKDIR
 terraform init
 terraform validate
 terraform workspace select $TF_WORKSPACE || terraform workspace new $TF_WORKSPACE
-terraform $TF_ARGS
+
+count=1
+while true; do
+    terraform $TF_ARGS
+    if(( $?==0 ))
+    then
+        break;
+    elif(( $?!=0 && $count>=$RETRY))
+    then
+        exit 1
+    else
+        num=$(($count + 1))
+        sleep $SLEEP_INTERVAL
+        echo "Retrying again"
+    fi
+done
