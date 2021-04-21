93ff988d6aee64639b285323a5f17cc916991dab
TechG <TechG>
2021-04-21 20:03:03 +0530

    refactored

--- terraform/entrypoint.sh
+++ terraform/entrypoint.sh
@@ -20,9 +20,6 @@ while true; do
         break;
     elif(( $CMD_EXIT_STATUS!=0 && $count>=$RETRY))
     then
-        # Work around for known issue https://registry.terraform.io/modules/Young-ook/eks/aws/1.4.3#unauthorized
-        [ $(terraform state list) == "module.eks.kubernetes_config_map.aws_auth[0]" ]
-        
         REMAINING_STATE_COUNT=$(terraform state list | wc -l |  awk '{ print $1 }')
         if(( $REMAINING_STATE_COUNT==1 ))
         then
@@ -33,6 +30,7 @@ while true; do
             if [ "$REMAINING_STATE" == "$AWS_AUTH_TF_RESOURCE" ]; then
                 echo "Deleting \"$AWS_AUTH_TF_RESOURCE\" from TF state"
                 terraform state rm $AWS_AUTH_TF_RESOURCE
+                echo "Work around for known issue: https://registry.terraform.io/modules/Young-ook/eks/aws/1.4.3#unauthorized"
             else
                 exit 1    
             fi    
